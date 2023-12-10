38d4f8c911a50ddadc2ca89d62d1fca00c8c5689
Leyla Aliyeva <leyla.fa@code.edu.az>
2023-12-10 19:35:53 +0400

    ...

---
+++ EDefectiveTrade.Business/Services/Implementations/CityService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDefectiveTrade.Business.Services.Implementations
+{
+    public class CityService
+    {
+    }
+}
